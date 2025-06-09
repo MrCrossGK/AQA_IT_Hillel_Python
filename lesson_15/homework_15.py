4d494054c8e337e9b64e8afb308dddaa20e68bd4
MrCrossGK <crosspocker@gmail.com>
2025-06-09 22:45:09 +0300

    Complete homework_15

---
+++ lesson_15/homework_15.py
@@ -0,0 +1,41 @@
+import pytest
+class Rhombus:
+
+    def __init__(self, side_a: int, angle_a: int):
+        self.side_a = side_a
+        self.angle_a = angle_a
+
+    def __setattr__(self, name, value):
+        if name == "side_a" and value <= 0:
+            raise AttributeError("The side_a should be > 0!")
+        if name == "angle_a" and (value >= 180 or value <= 0):
+            raise AttributeError("The angle_a should be < 180 or > 0 degrees!")
+        super().__setattr__(name, value)
+
+    def angle_calc(self) -> int:
+        angle_b = 180 - self.angle_a
+        print(f"The angle_b is equal - {angle_b} degrees.")
+        return angle_b
+
+
+if __name__ == "__main__":
+    try:
+        figure_1 = Rhombus(10, 170).angle_calc()
+    except AttributeError:
+        print("You tried to SET the side_a < 0 or angle_a > 180 or < 0 degrees!")
+
+
+@pytest.mark.parametrize("side_a,angle_a,expected",
+                         [(20, 30, 150),
+                          (1, 179, 1),
+                          (-2, 55, AttributeError),
+                          (12, 181, AttributeError)])
+def test_success(side_a, angle_a, expected):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            Rhombus(side_a, angle_a)
+    else:
+        rhombus = Rhombus(side_a, angle_a)
+        result = rhombus.angle_calc()
+        assert result == expected
+

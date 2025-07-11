293b4caf9268b4ea0692a7030c2af720f04871df
MrCrossGK <crosspocker@gmail.com>
2025-07-11 04:52:39 +0300

    the last update

--- lesson_22/university.py
+++ lesson_22/university.py
@@ -10,8 +10,14 @@ class University:
         self.courses_list = ['Math', 'History', 'Geometry', 'English', 'Spanish', 'Physics']
         self.course_ids = [id for (id,) in session.query(Course.id).all()]
         self.stud_ids = [id for (id,) in session.query(Student.id).all()]
-        self.rand_stud_id = random.sample(self.stud_ids, k=1)
-        self.rand_course_id = random.sample(self.course_ids, k=1)
+
+    @property
+    def rand_stud_id(self):
+        return random.choice(self.stud_ids)
+
+    @property
+    def rand_course_id(self):
+        return random.choice(self.course_ids)
 
     def stud_assign(self):
         relation = []
@@ -38,31 +44,32 @@ class University:
 
     def update_student(self):
         rand_stud_name = Faker().name()
-        student = self.session.get(Student, self.rand_stud_id)
+        rand_id = self.rand_stud_id
+        student = self.session.get(Student, rand_id)
         if student:
             student.student_name = rand_stud_name
             self.session.commit()
-            print(f"Студент з id {self.rand_stud_id[0]} оновлений: {rand_stud_name}")
+            print(f"Студент з id {rand_id} оновлений: {rand_stud_name}")
         else:
             print("Студент не знайден!")
 
     def update_course(self):
         addit_courses = ['Astronomy', 'Rocket_building', 'Python']
         rand_addit_course = random.sample(addit_courses, k=1)
-        course = self.session.get(Course, self.rand_course_id[0])
+        course = self.session.get(Course, self.rand_course_id)
         if rand_addit_course not in self.courses_list:
             course.course_name = rand_addit_course[0]
             self.session.commit()
-            print(f"Курс с id {self.rand_course_id[0]} обновлён: {rand_addit_course[0]}")
+            print(f"Курс с id {self.rand_course_id} обновлён: {rand_addit_course[0]}")
         else:
             print("Курс вже присутній!")
 
     def delete_student(self):
-        student = self.session.get(Student, self.rand_stud_id[0])
+        student = self.session.get(Student, self.rand_stud_id)
         if student:
             self.session.query(Relation).filter_by(student_id=student.id).delete()
             self.session.delete(student)
             self.session.commit()
-            print(f"Студент з id {self.rand_stud_id[0]} видалений!")
+            print(f"Студент з id {self.rand_stud_id} видалений!")
         else:
             print("Студента не знайдено!")
